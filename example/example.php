f537d4885f30d71b1df7be73b56f9c4ae65c0083
janhuang <384099566@qq.com>
2015-11-27 14:52:00 +0800

    remove di

---
+++ example/example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 15/11/27
+ * Time: 下午2:29
+ * Github: https://www.github.com/janhuang
+ * Coding: https://www.coding.net/janhuang
+ * SegmentFault: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ * Gmail: bboyjanhuang@gmail.com
+ * WebSite: http://www.janhuang.me
+ */
+
+include __DIR__ . '/../vendor/autoload.php';
+
+
