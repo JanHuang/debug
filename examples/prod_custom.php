c0522d3163afa6399259cdcd7eb2d4100bd40372
janhuang <384099566@qq.com>
2016-05-11 10:20:38 +0800

    add prod_custom

---
+++ examples/prod_custom.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 15/8/8
+ * Time: 下午7:32
+ * Github: https://www.github.com/janhuang
+ * Coding: https://www.coding.net/janhuang
+ * SegmentFault: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ * Gmail: bboyjanhuang@gmail.com
+ * WebSite: http://www.janhuang.me
+ */
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use FastD\Debug\Debug;
+
+$debug = Debug::enable(false);
+
+class JsonException extends \FastD\Debug\Exceptions\Http\HttpException
+{
+    public function __construct(array $data)
+    {
+        parent::__construct(json_encode($data));
+    }
+
+    /**
+     * Returns the status code.
+     *
+     * @return int An HTTP response status code
+     */
+    public function getStatusCode()
+    {
+        return \FastD\Debug\Exceptions\Http\HttpExceptionInterface::HTTP_BAD_REQUEST;
+    }
+
+    /**
+     * Returns response headers.
+     *
+     * @return array Response headers
+     */
+    public function getHeaders()
+    {
+        return [
+            'Content-Type' => 'application/json; charset=utf-8'
+        ];
+    }
+}
+
+throw new JsonException(['name' => 'hello world']);
