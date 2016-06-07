79b2d1d008605547ca09f46a2eb5c26a88d52649
janhuang <384099566@qq.com>
2016-06-07 15:18:31 +0800

    change custom

--- examples/page.php
+++ examples/page.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: janhuang
- * Date: 16/4/21
- * Time: 下午11:10
+ * Date: 15/8/8
+ * Time: 下午7:32
  * Github: https://www.github.com/janhuang
  * Coding: https://www.coding.net/janhuang
  * SegmentFault: http://segmentfault.com/u/janhuang
@@ -14,10 +14,34 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-$debug = \FastD\Debug\Debug::enable();
+use FastD\Debug\Debug;
 
-class PageException extends \FastD\Debug\Exceptions\Http\HttpException
+$debug = Debug::enable(false);
+
+class PageException extends \FastD\Debug\Exceptions\HttpException
 {
+    /**
+     * Returns the status code.
+     *
+     * @return int An HTTP response status code
+     */
+    public function getStatusCode()
+    {
+        return 400;
+    }
+
+    /**
+     * Returns response headers.
+     *
+     * @return array Response headers
+     */
+    public function getHeaders()
+    {
+        return [
+            'Content-Type' => 'application/json; charset=utf-8'
+        ];
+    }
+
     /**
      * Returns response content.
      *
@@ -25,8 +49,8 @@ class PageException extends \FastD\Debug\Exceptions\Http\HttpException
      */
     public function getContent()
     {
-        return file_get_contents(__DIR__ . '/demo.html');
+        return '{"name": "janhuang"}';
     }
 }
 
-throw new PageException('custom');
+throw new PageException();
